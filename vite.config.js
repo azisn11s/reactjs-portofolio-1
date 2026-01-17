afcbbce3756887c25ca7e98341e40b19a9d5bcbd
Eiger Care Omni <no-reply@eigerindo.co.id>
2026-01-17 15:10:32 +0700

    Update for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reactjs-portofolio-1', // Ganti dengan nama repository GitHub kamu
 })
